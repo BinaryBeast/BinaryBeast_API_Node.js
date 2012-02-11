b6b65340d30e7e90890232bc02a0a6e82e5c59b0
Brandon <brandon@binarybeast.com>
2012-02-11 00:50:14 -1000

    Slight folder restructure

---
+++ binarybeast/index.js
@@ -0,0 +1,173 @@
+/**
+ * BinaryBeast API written for Node.js
+ *
+ * @author Brandon Simmons
+ *
+ * You can find documentation on the API itself at http://binarybeast.com/api/info 
+ *
+ * Feel free to send any questions/suggestions to contact@binarybeast.com
+ */
+
+
+
+
+//Modules containing wrapper methods and other depdencies
+var 	tournament	= require('./tournament'),
+	https		= require('https')
+//	querystring	= require('querystring');
+
+
+/**
+ * BinaryBeast constructor
+ */
+var BinaryBeast = function(api_key) {
+
+	this.api_key = api_key;
+
+	this.tournament = new tournament(this);	
+}
+/**
+ * Properties
+ */
+BinaryBeast.prototype = {
+
+	//ghetto 'constants'
+	VERSION: 		'0.0.1',
+	//
+	BRACKET_GROUPS:		0,
+	BRACKET_WINNERS:	1,
+	BRACKET_LOSERS:		2,
+	BRACKET_FINALS:		3,
+	BRACKET_BRONZE:		4,
+	//
+	ELIMINATION_SINGLE:	1,
+	ELIMINATION_DOUBLE:	2,
+	//
+	TOURNEY_TYPE_BRACKETS:	0,
+	TOURNEY_TYPE_CUP:	1,
+
+	//Store the api key
+	api_key: 		null,
+
+	//Wrapper modules
+	tournament: 		null,
+	team: 			null,
+	country:		null,
+	game:			null,
+
+	//jQuery extend();
+	extend:			null
+
+};
+
+/**
+ * Make a service call
+ * 
+ * @param svc		A Service name (ie 'Tourney.TourneyCreate.Create')
+ * @param args		An associate array of arguments ie {'Title': 'My Node API Test!'}
+ *
+ * 
+ */
+BinaryBeast.prototype.call = function(svc, args, callback) {
+
+	//Add a few things to the query, then compile it into a query string using querystring
+	args = this.extend(args, {
+		api_use_underscores:		1,
+		api_key:			this.api_key,
+		api_service:			svc
+	});
+	//var query = querystring.stringify(args);
+	var query = this.buildQuery(args);
+
+	//Compile the request information
+        var options = {
+                host:   'binarybeast.com',
+                port:   443,
+                path:   '/api',
+                method: 'POST',
+		headers: {
+			'Content-Type': 	'application/x-www-form-urlencoded',
+			'Content-Length':	query.length,
+			'User-Agent':		'BinaryBeast API Node.js (v' + this.VERSION + ')'
+		}
+        };
+
+	//Setup the request
+	var request = https.request(options, function(response) {
+		response.setEncoding('utf8');
+
+		var body = '';
+		response.on('data', function(chunk) {
+			body += chunk;
+		});
+		response.on('end', function() {
+			//Success!! Invoke the callback if it's actually a valid function
+			if(typeof callback == 'function') {
+				callback(JSON.parse(body));
+			}
+		});
+	});
+
+	//OH NOES
+	request.on('error', function(error) {
+		//Send an error to the callback, granted that it's a valid function
+		if(typeof callback == 'function') {
+			callback({result: 500, error: error.message});
+		}
+	});
+
+	//gogogo
+	request.write(query);
+	request.end();
+}
+
+/**
+ * I'm spoiled by jquery's .extend, so we're duplicating it here
+ * used by the service wrapper modules to allow users to submit options arrays, and retain simple default values
+ * 
+ * In this method, obj2 gets priority - any duplicate values will result in the value from obj2
+ */
+BinaryBeast.prototype.extend = function(obj1, obj2) {
+
+	if(typeof obj1 != 'object') obj1 = [obj1];
+	if(typeof obj2 != 'object') obj2 = [obj2];
+
+	for(var x in obj2) obj1[x] = obj2[x];
+	return obj1;
+}
+
+/**
+ * BinaryBeast.buildQuery(obj args)
+ *
+ * Unfortunately, the popular querystring does not currently seem to support "stringifying" complex objects
+ * so I'm writing my own query parser
+ *
+ * @return {string}
+ */
+BinaryBeast.prototype.buildQuery = function(args) {
+
+	var out = '';
+
+	for(var x in args) {
+		if(typeof args[x] == 'object') {
+			for(var y in args[x]) {
+				out += (out == '' ? '' : '&') +  x + '[' + y + ']=' + getValue(args[x][y]);
+			}
+		}
+		else out += (out == '' ? '' : '&') + x + '=' + getValue(args[x]);
+	}
+
+	/**
+	 * Very simply returns the value to add to the query
+	 * For two reasons: escaping special characters, and converting booleans
+	 */
+	function getValue(val) {
+		if(typeof val == 'boolean') return val == true ? 1 : 0;
+		return escape(val);
+	}
+
+	return out;
+};
+
+
+module.exports = BinaryBeast;
---
+++ binarybeast/tournament.js
@@ -0,0 +1,190 @@
+/**
+ * This module provides the main BinaryBeast node.js module with wrapper methods
+ * used to manipulate tournaments
+ *
+ * @author Brandon Simmons
+ * @version 0.01
+ *
+ * @see http://binarybeast.com/api/info
+ */
+
+
+
+
+/**
+ * Constructor
+ * 
+ * Argument accepts a reference to the API class, as it does the actual calls
+ * This wrapper class only containers a list of service names + arguments
+ */
+var tournament = function(bb) {
+	this.bb = bb;
+}
+
+/**
+ * Properties
+ */
+tournament.prototype = {
+
+	//Reference to the main API Class
+	bb: 		null
+};
+
+
+/**
+ * bb.tournament.create(options, callback)
+ * 
+ * Wrapper method for creating a tournament
+ *
+ * Visit http://wiki.binarybeast.com/index.php?title=Tourney.TourneyCreate.Create
+ * for a list of available options (don't forget to use underscores instead of camelCase)
+ * so TypeID should be type_id
+ *
+ * @param options	Array of options
+ * @param callback	What to do with the result (expect 1 argument, a data object)
+ *
+ * @return {null}
+ */
+tournament.prototype.create = function(options, callback) {
+
+	//Extend default settings with whatever the user gives us
+	options = this.bb.extend({
+		title: 		'Node API Test',
+		type_id:	this.bb.TOURNEY_TYPE_BRACKETS,
+		elimination:	this.bb.ELIMINATION_SINGLE
+	}, options);
+
+	this.bb.call('Tourney.TourneyCreate.Create', options, callback);
+};
+
+
+/**
+ * bb.tournament.delete(tourney_id, callback)
+ *
+ * Delete a tournament
+ *
+ * @param tourney_id
+ * @param callback(result)
+ *
+ * @return {null}
+ */
+tournament.prototype.delete = function(tourney_id, callback) {
+	this.bb.call('Tourney.TourneyDelete.Delete', {'tourney_id':tourney_id}, callback);
+};
+
+
+/**
+ * bb.tournament.update(tourney_id, options, callback)
+ *
+ * Update tournament settings
+ * 
+ * See bb.tournament.create for a list of available options, they are very nearly the same with obvious exceptions
+ *
+ * @param tourney_id
+ * @param options
+ * @param callback(result)
+ *
+ * @return {null}
+ */
+tournament.prototype.update = function(tourney_id, options, callback) {
+	options = bb.extend(options, {'tourney_id':tourney_id});
+	bb.call('Tourney.TourneyDelete.Delete', options, callback);
+};
+
+/**
+ * bb.tournament.list([options], callback(result))
+ *
+ * This wrapper method returns a list of tournaments in your account
+ *
+ * Available Options:
+ * 	string filter	Just like it sounds, filter your results with a keyword / phrase
+ *	int page_size	aka limit the number of results
+ *	bool private	By default it WILL include your private tournaments, but you can disable that
+ *
+ * @param options
+ * @param callback(result)
+ *
+ * @return {null}
+ */
+tournament.prototype.list = function(options, callback) {
+
+	if(typeof options == 'function') {
+		callback = options;
+		options = {};
+	}
+	options = this.bb.extend({
+		filter: 	null,
+		page_size:  	30,
+		private: 	true
+	}, options);
+
+	this.bb.call('Tourney.TourneyList.Creator', options, callback);
+};
+
+/**
+ * bb.tournament.round_update(tourney_id, bracket, round, best_of, map, date, callback)
+ *
+ * Change the format of a round within a tournament (best of, map, and date)
+ * 
+ * This function also works to create the details - even if they have not yet been provided
+ *
+ * @param tourney_id
+ * @param bracket	which bracket the round effects - ie 0 = groups, 1 = winners (there are class constants for these values (ie bb.BRACKET_WINNERS, bb.BRACKET_BRONZE)
+ * @param round		0 = first round
+ * @param best_of
+ * @param map
+ * @param date		Just a string, no strict format is required
+ * @param callback(result)
+ *
+ * @return {null}
+ */
+tournament.prototype.round_update = function(tourney_id, bracket, round, best_of, map, date, callback) {
+
+	this.bb.call('Tourney.TourneyRound.Update, {
+		'tourney_id':		tourney_id,
+		'bracket':		bracket,
+		'round':		round,
+		'best_of':		best_of,
+		'map':			map,
+		'date':			date
+	}, callback);
+};
+
+/**
+ * bb.tournament.round_update_batch(tourney_id, bracket, best_ofs, maps, dates, callback) 
+ * 
+ * the round_update function is fine and all.. but not incredibly effecient for large tournaments with
+ * many rounds and brackets, this method llows you to update all rounds with one call, by passing in
+ * a simple array for each value
+ *
+ *
+ * The arrays should each be indexed by round
+ *
+ * For example... var best_ofs = [1, 1, 3, 5]; indicates 
+ * 	round 1 = best of 1
+ * 	round 2 = best of 1
+ *	round 3 = best of 3
+ * 	round 4 = best of 5
+ * 
+ * Same goes for maps[] and dates[]
+ *
+ * @param tourney_id
+ * @param bracket
+ * @param best_ofs
+ * @param maps
+ * @param dates
+ * @param callback(result)
+ *
+ * @return void
+ */
+tournament.prototype.round_update_batch = function(tourney_id, bracket, best_ofs, maps, dates, callback) {
+	this.bb.call('Tourney.TourneyRound.BatchUpdate', {
+		'tourney_id':		tourney_id,
+		'bracket':		bracket,
+		'best_ofs':		best_ofs,
+		'maps':			maps,
+		'dates':		dates
+	}, callback);
+}
+
+module.exports = tournament;
