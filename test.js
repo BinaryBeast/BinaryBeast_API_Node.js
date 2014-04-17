47c68b4dce9498825be113fa5cd4f45d2c5316af
Brandon Simmons <brandon@binarybeast.com>
2014-04-17 04:29:50 -0400

    Implements the new Cache  handler module in the main BinaryBeast class
    Also makes a few minor changes to package.json and the temp test.js script

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
-var bb = require('./lib/index')('e17d31bfcbedd1c39bcb018c5f0d0fbf.4dcb36f5cc0d74.24632846');
-var util = require('util');
+var bb = new (require('./lib/index'))('e17d31bfcbedd1c39bcb018c5f0d0fbf.4dcb36f5cc0d74.24632846');
+var
+    util = require('util');
+    utils = require('./lib/core/utils');
+
+//Setup the cache handler
+/*bb.cache.set_engine('redis', {
+    db: 1
+});
+*/
+bb.cache.set_engine('couchdb', {
+    db: 'bb_api_node_cache'
+});
 
 var handler = function() {
     util.log('api response: ' + util.inspect(arguments));
 };
 
 
-var tour = new bb.tournament('x13081612');
-tour.load(handler, bb);
+var tournament = new bb.tournament('xUSFIV1404040');
+tournament.load(handler, bb);
+
+
+module.exports = tournament;
