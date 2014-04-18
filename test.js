b8a8138a7b1c7a41dbf8ec376219f68faf3a0a5c
Brandon Simmons <brandon@binarybeast.com>
2014-04-18 06:10:12 -0400

    Minor test script changes

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var bb = new (require('./lib/index'))('e17d31bfcbedd1c39bcb018c5f0d0fbf.4dcb36f5cc0d74.24632846');
 var
-    util = require('util');
-    utils = require('./lib/core/utils');
+    _       = require('underscore'),
+    util    = require('util'),
+    utils   = require('./lib/core/utils');
 
 //Setup the cache handler
 /*bb.cache.set_engine('redis', {
@@ -12,13 +13,45 @@ bb.cache.set_engine('couchdb', {
     db: 'bb_api_node_cache'
 });
 
-var handler = function() {
-    util.log('api response: ' + util.inspect(arguments));
+var handle_tour = function(tournament, error) {
+    if(!tournament) {
+        utils.error('Error loading tournament', error);
+        return;
+    }
+
+    utils.log('tournament load', _.pick(tournament, [
+        'name', 'id', 'status',
+        'type', 'elimination',
+        'game',
+        'teams_confirmed_count',
+        'group_count', 'group_mode'
+    ]));
+
+    tournament.participants(handle_participants);
+
+};
+var handle_participants = function(participants, error) {
+    if(!participants) {
+        utils.error('Error loading participants', error);
+        return;
+    }
+
+    utils.log('Successfully loaded ' + participants.length + ' participants');
+    _.each(participants, function(participant) {
+
+        utils.log('tournament load', _.pick(participant, [
+            'name', 'id', 'status',
+            'country', 'country_flag',
+            'group',
+            'race_id', 'race', 'race_icon',
+            'match_wins', 'match_losses'
+        ]));
+
+    });
 };
 
 
-var tournament = new bb.tournament('xUSFIV1404040');
-tournament.load(handler, bb);
+var tournament = new bb.tournament('xHotS1404181');
 
-
-module.exports = tournament;
+utils.log('loading xHotS1404181');
+tournament.load(handle_tour);
