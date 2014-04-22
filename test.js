a256ae3e2f67f9aba76fd836f132ab9e33d68d21
Brandon Simmons <brandon@binarybeast.com>
2014-04-22 05:17:25 -0400

    Minor progress with the rebuild.. and adds a working example for pulling dual tournament group rounds standings / results

--- test.js
+++ test.js
@@ -5,14 +5,15 @@ var
     utils   = require('./lib/core/utils');
 
 //Setup the cache handler
-bb.cache.set_engine('redis', {
+/*bb.cache.set_engine('redis', {
     db: 2,
     port: 6370
+});*/
+///*
+bb.cache.set_engine('couchdb', {
+    db: 'bb_api_node_cache'
 });
 //*/
-/*bb.cache.set_engine('couchdb', {
-    db: 'bb_api_node_cache'
-});*/
 
 var handle_tour = function(tournament, error) {
     if(!tournament) {
@@ -28,6 +29,7 @@ var handle_tour = function(tournament, error) {
         'group_count', 'group_mode'
     ]));
 
+    utils.log('Loading group round brackets... ');
     //tournament.participants(handle_participants);
     tournament.draw_groups(handle_groups);
 
@@ -54,11 +56,37 @@ var handle_participants = function(participants, error) {
     });
 };
 
-var handle_groups = function(groups, error) {
+var handle_groups = function(groups, error, options) {
     if( !groups ) {
-        utils.error('Error fetching groups', error);
+        utils.error('Error fetching groups', {
+            error: error,
+            groups: groups,
+            options: options
+        });
         return;
     }
+
+    utils.log('Successfully fetched group standings and results', groups);
+    _.each(groups, function(group, label) {
+        utils.log(' ** group ' + label + ' **', null, false, true);
+
+        _.each(group.brackets, function(rounds, bracket) {
+            utils.log('\n_' + bracket + ' bracket_\n', null, false, true);
+
+            _.each(rounds, function(matches, round) {
+                utils.log(' -round ' + (parseInt(round) + 1) + '-', null, false, true);
+
+                _.each(matches, function(match, index) {
+                    utils.log('match ' + (parseInt(index) + 1) + '\n', {
+                        'team1': match.team ? _.pick(match.team, 'id', 'display_name', 'race', 'country') : null,
+                        'team2': match.team ? _.pick(match.opponent, 'id', 'display_name', 'race', 'country') : null,
+                        'winner': match.winner()
+                    }, false, true);
+                });
+            })
+        });
+    });
+
 };
 
 
